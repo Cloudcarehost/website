666f259cbf15e2681d694bf1b4f74b8f30d15a15
Rohit Bachhav <rohitbachhav80@gmail.com>
2025-10-12 02:28:11 +0530

    Development page changes

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -226,4 +226,24 @@ class BlogController extends Controller
 
         return view('user.single-blog', compact('blog', 'relatedBlogs'));
     }
+
+     public function showDevelopment()
+    {
+        $categorySlug = 'development';
+
+        $category = Category::where('slug', $categorySlug)->first();
+        
+        if (! $category) {
+            $blogs = collect();
+            return view('user.development', compact('blogs'));
+        }
+
+        $blogs = Blog::where('type', 'blog')
+            ->where('category_id', $category->id)
+            ->whereNotNull('published_at')
+            ->limit(3)
+            ->get();
+
+        return view('user.development', compact('blogs'));
+    }
 }
